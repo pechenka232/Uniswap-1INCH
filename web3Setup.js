e5643c1b0aac20ae93d0bdef50e0274ddb492608
pechenka232 <strekalovskijdemid@gmail.com>
2024-10-20 16:27:16 +0800

    Update web3Setup.js

--- web3Setup.js
+++ web3Setup.js
@@ -1,4 +1,4 @@
 const Web3 = require('web3');
-const web3 = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
+const web3 = new Web3('https://mainnet.infura.io/v3/');
 
 module.exports = web3;
